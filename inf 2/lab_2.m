4980342a24c8bcd628f40b6551f095642f405453
Astolfo <migue19sep@gmail.com>
2023-09-12 15:18:18 -0500

    Segunda parte

--- inf 2/lab_2.m
+++ inf 2/lab_2.m
@@ -1,5 +1,4 @@
-% funcion para realimentacion feedback(G(s),H(s))
-% lsim(g,x,t) con x=t asi lsim(g,t,t)
+%% Punto 4.1
 close all;clear;
 T=[0.5 1 4];
 k=[0.6 1 3 9];
@@ -50,3 +49,63 @@ for a=1:3
         u=1;
     end
 end
+%% Punto 4.2 
+% a
+psi=0.3;
+wn=[0.9 3];
+t=0:0.1:30;
+for i=1:2
+    figure(4)
+    h=tf(wn(i)^2,[1 2*psi*wn(i) wn(i)^2]);
+    y=step(h,t);
+    plot(t,y,LineWidth=2)
+    hold on
+end
+legend("wn: 0.9","wn: 3")
+title("Respuesta al escalon funcion segundo orden con psi: 0.3")
+% b
+t=0:0.1:8;
+psi=[0.2 0.7 2];
+wn=7;
+for i=1:3
+    figure(5)
+    h=tf(wn^2,[1 2*psi(i)*wn wn^2]);
+    y=step(h,t);
+    plot(t,y,LineWidth=2)
+    hold on
+end
+legend("psi: 0.2","psi: 0.3","psi: 2")
+title("Respuesta al escalon funcion segundo orden con wn: 7")
+%% 4.3
+t=0:0.1:5;
+k=[0.35 1 1.5 6];
+psi=0.6;
+wn=4;
+h=tf(wn^2,[1 2*psi*wn wn^2]);
+for n=1:3
+    figure(6+n)
+    
+    for i=1:4
+        hr=feedback(h,k(i));
+        if n==1
+            s=step(hr,t);
+        elseif n==2
+            s=lsim(hr,t,t);
+        else
+            s=impulse(hr,t);
+        end
+        plot(t,s,LineWidth=2)
+        title("Respuesta "+tit(n))
+        hold on
+        nombre{i}="K: "+num2str(k(i));
+        if i==4
+            legend(nombre)
+        end
+        
+    end
+end
+%% 4.4
+
+
+
+
