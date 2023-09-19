6af924e23c06206d5833f52197c8725638b36eda
Miguel <migue19sep@gmail.com>
2023-09-18 19:12:01 -0500

    Comentado

--- inf 2/lab_2.m
+++ inf 2/lab_2.m
@@ -1,107 +1,117 @@
-%% Punto 4.1
-close all;clear;
-T=[0.5 1 4];
-k=[0.6 1 3 9];
-t=0:0.1:10;
-u=1;
+%% Punto 4.1: Análisis de Sistemas de Primer Orden
+close all;
+clear;
+T = [0.5 1 4]; % Valores de constante de tiempo (tau)
+k = [0.6 1 3 9]; % Valores de constante de realimentación (K)
+t = 0:0.1:10; % Vector de tiempo
+u = 1;
 n = cell(1, 3);
-nombre=cell(1,4);
-tit=["escalon","rampa","impuslo"];
-for a=1:3
-    for i=1:3 
-        h=tf(1,[T(i) 1]);
+nombre = cell(1, 4);
+tit = ["escalon", "rampa", "impulso"];
+
+% Bucle para sistemas de primer orden
+for a = 1:3
+    for i = 1:3
+        h = tf(1, [T(i) 1]); % Función de transferencia
         figure(a)
-        subplot(2,2,1)
-        title("Respuesta "+tit(a))
-        if a==1
-            s=step(h,t);
-        elseif a==2
-            s=lsim(h,t,t);
+        subplot(2, 2, 1)
+        title("Respuesta " + tit(a))
+        if a == 1
+            s = step(h, t); % Respuesta al escalón
+        elseif a == 2
+            s = lsim(h, t, t); % Respuesta a la rampa
         else
-            s=impulse(h,t);
+            s = impulse(h, t); % Respuesta al impulso
         end
     
-        plot(t,s)
+        plot(t, s)
         hold on
-        n{i}="T: "+num2str(T(i));
-        if i==3
-        legend(n)
+        n{i} = "T: " + num2str(T(i));
+        if i == 3
+            legend(n)
         end
-        for o=1:4
-            nombre{u}="K: "+num2str(k(o));
-            subplot(2,2,i+1)
-            g=feedback(h,tf(k(o)));
-            title("Respuesta "+tit(a)+" lazo cerrado"+" para T: "+num2str(T(i)))
-            if a==1
-                sg=step(g,t);
-            elseif a==2
-                sg=lsim(g,t,t);
+        for o = 1:4
+            nombre{u} = "K: " + num2str(k(o));
+            subplot(2, 2, i + 1)
+            g = feedback(h, tf(k(o))); % Sistema en lazo cerrado
+            title("Respuesta " + tit(a) + " lazo cerrado" + " para T: " + num2str(T(i)))
+            if a == 1
+                sg = step(g, t);
+            elseif a == 2
+                sg = lsim(g, t, t);
             else
-                sg=impulse(g,t);
+                sg = impulse(g, t);
             end
-            plot(t,sg)
+            plot(t, sg)
             hold on
-            if o==4
-            legend(nombre)
+            if o == 4
+                legend(nombre)
             end
-            u=u+1;
+            u = u + 1;
         end
-        u=1;
+        u = 1;
     end
 end
-%% Punto 4.2 
-% a
-psi=0.3;
-wn=[0.9 3];
-t=0:0.1:30;
-for i=1:2
+
+%% Punto 4.2: Análisis de Sistemas de Segundo Orden
+% a) Variación de wn
+psi = 0.3; % Factor de amortiguamiento relativo
+wn = [0.9 3]; % Variación de frecuencia natural no amortiguada
+t = 0:0.1:30;
+
+% Bucle para sistemas de segundo orden - parte a
+for i = 1:2
     figure(4)
-    h=tf(wn(i)^2,[1 2*psi*wn(i) wn(i)^2]);
-    y=step(h,t);
-    plot(t,y)
+    h = tf(wn(i)^2, [1 2*psi*wn(i) wn(i)^2]); % Función de transferencia
+    y = step(h, t); % Respuesta al escalón
+    plot(t, y)
     hold on
 end
-legend("wn: 0.9","wn: 3")
+legend("wn: 0.9", "wn: 3")
 title("Respuesta al escalon funcion segundo orden con psi: 0.3")
-% b
-t=0:0.01:8;
-psi=[0.2 0.7 2];
-wn=7;
-for i=1:3
+
+% b) Variación de psi
+t = 0:0.01:8;
+psi = [0.2 0.7 2]; % Variación de factor de amortiguamiento 
+wn = 7;
+
+% Bucle para sistemas de segundo orden - parte b
+for i = 1:3
     figure(5)
-    h=tf(wn^2,[1 2*psi(i)*wn wn^2]);
-    y=step(h,t);
-    plot(t,y)
+    h = tf(wn^2, [1 2*psi(i)*wn wn^2]); % Función de transferencia
+    y = step(h, t); % Respuesta al escalón
+    plot(t, y)
     hold on
 end
-legend("psi: 0.2","psi: 0.3","psi: 2")
+legend("psi: 0.2", "psi: 0.3", "psi: 2")
 title("Respuesta al escalon funcion segundo orden con wn: 7")
-%% 4.3
-t=0:0.01:5;
-k=[0.35 1 1.5 6];
-psi=0.6;
-wn=4;
-h=tf(wn^2,[1 2*psi*wn wn^2]);
-for n=1:3
-    figure(6+n)
-    
-    for i=1:4
-        hr=feedback(h,k(i));
-        if n==1
-            s=step(hr,t);
-        elseif n==2
-            s=lsim(hr,t,t);
+
+%% Punto 4.3: Análisis de Sistemas de Segundo Orden con Realimentación
+t = 0:0.01:5;
+k = [0.35 1 1.5 6]; % Valores de constante de realimentación (K)
+psi = 0.6;
+wn = 4;
+h = tf(wn^2, [1 2*psi*wn wn^2]); % Función de transferencia
+
+% Bucle para sistemas de segundo orden - parte c
+for n = 1:3
+    figure(6 + n)
+    for i = 1:4
+        hr = feedback(h, k(i)); % Sistema en lazo cerrado
+        if n == 1
+            s = step(hr, t); % Respuesta al escalón
+        elseif n == 2
+            s = lsim(hr, t, t); % Respuesta a la rampa
         else
-            s=impulse(hr,t);
+            s = impulse(hr, t); % Respuesta al impulso
         end
-        plot(t,s)
-        title("Respuesta "+tit(n))
+        plot(t, s)
+        title("Respuesta " + tit(n))
         hold on
-        nombre{i}="K: "+num2str(k(i));
-        if i==4
+        nombre{i} = "K: " + num2str(k(i));
+        if i == 4
             legend(nombre)
         end
-        
     end
 end
 
--- inf 2/programa_4_4.m
+++ inf 2/programa_4_4.m
@@ -1,67 +1,84 @@
 while true
-mp=input("Por favor ingrese un valor para el porcentaje de sobrepaso," + ...
-        "el valor no debe ser superior a 30% ni menor a 10%. O ingrese 0 para" + ...
-        " salir: " );
-close all
-if length(mp)==0
-    display("Por favor ingrese una valor")
-elseif mp==0
-    break
-elseif mp<10
-    display("Por favor ingrese un valor superior o igual a 10")
-elseif mp>30
-    display("Por favor ingrese una valor inferior o igual a 30")
-elseif mp>=10 && mp<=30
-        tp=input("Ingrese el tiempo donde desea que se encuentre el valor " + ...
-            "máximo, debe ser un valor superior a 1: ");
-        if length(tp)==0
-            display("Por favor ingrese un valor")
-        elseif tp>1
-            t=0:0.01:tp*5;
-            psi=sqrt((log(mp/100)^2)/(pi^2+(log(mp/100)^2)));
-            wn=pi/(tp*sqrt(1-psi^2));
-            Ts=3/(wn*psi);
-            if Ts>7
-                display("Para este sobrepaso ingrese un valor de tiempo pico " + ...
-                    "menor a "+num2str((psi*7*pi)/(3*sqrt(1-psi^2))))
+    % Solicitar al usuario que ingrese el valor de Mp (porcentaje de sobrepaso)
+    mp=input("Por favor ingrese un valor para el porcentaje de sobrepaso," + ...
+            "el valor no debe ser superior a 30% ni menor a 10%. O ingrese 0 para" + ...
+            " salir: " );
+    close all;
+    % Validar si el valor de Mp está vacío
+    if length(mp)==0
+        display("Por favor ingrese una valor")
+    % Salir del bucle si Mp es igual a 0
+    elseif mp==0
+        break
+    % Validar si Mp está por debajo del rango (menor a 10%)
+    elseif mp<10
+        display("Por favor ingrese un valor superior o igual a 10")
+    % Validar si Mp está por encima del rango (mayor a 30%)
+    elseif mp>30
+        display("Por favor ingrese una valor inferior o igual a 30")
+    % Si Mp está dentro del rango (entre 10% y 30%)
+    elseif mp>=10 && mp<=30
+            % Solicitar al usuario que ingrese el valor de Tp y validar
+            tp=input("Ingrese el tiempo donde desea que se encuentre el valor " + ...
+                "máximo, debe ser un valor superior a 1: ");
+            % Validar si el valor de Tp es nulo o no cumple con la condición
+            if length(tp)==0
+                display("Por favor ingrese un valor")
+            elseif tp>1
+                t=0:0.01:tp*5;
+                % Calcular los parámetros del sistema de segundo orden
+                psi=sqrt((log(mp/100)^2)/(pi^2+(log(mp/100)^2)));
+                wn=pi/(tp*sqrt(1-psi^2));
+                Ts=3/(wn*psi);
+                % Verificar si Ts es mayor que 7 segundos
+                if Ts>7
+                    display("Para este sobrepaso ingrese un valor de tiempo pico " + ...
+                        "menor a "+num2str((psi*7*pi)/(3*sqrt(1-psi^2))))
+                else
+                    h=tf(wn^2,[1 2*psi*wn wn^2]) % Función de transferencia
+                    figure(1)
+                    y=step(h,t); %Respuesta al escalón
+                    [maxv,maxi]=max(y);     
+                    % Calcular y mostrar el sobrepaso máximo (Mp)
+                    display("Sobrepaso máximo: ")                    
+                    Mp=(abs(maxv-1)/1)*100                    
+                    b=atan(sqrt(1-psi^2)/psi);
+                    % Calcular y mostrar el ángulo b para el tiempo de levantamiento (Tr)
+                    display("Tiempo de levantamiento: ")                    
+                    Tr=(pi-b)/(wn*sqrt(1-psi^2))
+                    dis=abs(0.5-y);
+                    cerca=find(dis==min(dis));
+                    % Encontrar y mostrar el tiempo de retardo (Td)
+                    display("Tiempo de retardo: ")                    
+                    Td=t(cerca)
+                    % Mostrar el tiempo de asentamiento (Ts)
+                    display("Tiempo de asentamiento: ")
+                    Ts
+                    plot(t,y)
+                    hold on 
+                    grid on
+                    % Marcar el punto del sobrepaso máximo
+                    plot(t(maxi),maxv, "r*",MarkerSize=10)
+                    text(t(maxi),maxv+0.1,"Sobrepaso maximo")
+                    % Marcar el punto del tiempo de retardo
+                    plot(Td,y(cerca), "r*",MarkerSize=10)
+                    text(Td,y(cerca),"Tiempo de retardo")
+                    % Marcar el punto del tiempo de levantamiento
+                    dis=abs(Tr-t);
+                    cerca=find(dis==min(dis));
+                    plot(t(cerca),y(cerca), "r*",MarkerSize=10)
+                    text(t(cerca),y(cerca)-0.1,"Tiempo de levantamiento")
+                    % Marcar el punto del tiempo de asentamiento
+                    dis=abs(Ts-t);
+                    cerca=find(dis==min(dis));
+                    plot(t(cerca),y(cerca), "r*",MarkerSize=10)
+                    text(t(cerca),y(cerca),"Tiempo de asentamiento")
+                    hold off
+                end
             else
-                h=tf(wn^2,[1 2*psi*wn wn^2])
-                figure(1)
-                y=step(h,t);
-                [maxv,maxi]=max(y);
-                display("Sobrepaso máximo: ")
-                Mp=(abs(maxv-1)/1)*100
-                b=atan(sqrt(1-psi^2)/psi);
-                display("Tiempo de levantamiento: ")
-                Tr=(pi-b)/(wn*sqrt(1-psi^2))
-                dis=abs(0.5-y);
-                cerca=find(dis==min(dis));
-                display("Tiempo de retardo: ")      
-                Td=t(cerca)
-                display("Tiempo de asentamiento: ")
-                Ts
-                plot(t,y)
-                hold on 
-                grid on
-                plot(t(maxi),maxv, "r*",MarkerSize=10)
-                text(t(maxi),maxv+0.1,"Sobrepaso maximo")
-                plot(Td,y(cerca), "r*",MarkerSize=10)
-                text(Td,y(cerca),"Tiempo de retardo")
-                dis=abs(Tr-t);
-                cerca=find(dis==min(dis));
-                plot(t(cerca),y(cerca), "r*",MarkerSize=10)
-                text(t(cerca),y(cerca)-0.1,"Tiempo de levantamiento")
-                dis=abs(Ts-t);
-                cerca=find(dis==min(dis));
-                plot(t(cerca),y(cerca), "r*",MarkerSize=10)
-                text(t(cerca),y(cerca),"Tiempo de asentamiento")
-                hold off
+                display("Ingrese un valor valido, recuerde que debe ser superior a 1")
             end
-        else
-            display("Ingrese un valor valido, recuerde que debe ser superior a 1")
-        end
-else
-    display("Ingrese un valor numerico")
+    else
+        display("Ingrese un valor numerico")
+    end
 end
-end
-
