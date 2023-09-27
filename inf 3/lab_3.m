7503da63c0656494834858db758029d2e725f13e
Astolfo <migue19sep@gmail.com>
2023-09-27 08:02:04 -0500

    se acabo

--- inf 3/lab_3.m
+++ inf 3/lab_3.m
@@ -1,6 +1,6 @@
 %% 4.1 a
 J=2;B=7;
-H=tf(1,[J B 0]);
+H=tf(1,[J B 0])
 rlocus(H)
 grid on
 %% b
@@ -14,3 +14,14 @@ H1=feedback(H,12.5)
 damp(H1)
 H1=feedback(H,5)
 damp(H1)
+%% 4.2
+syms x
+ecu=sym2poly(expand(x*(x+1.5)*(x+2)*(x+3)));
+G=tf(1,ecu)
+rlocus(G)
+%% b
+rltool(G)
+%% c
+G1=feedback(G,3)
+damp(G1)
+step(G1)
